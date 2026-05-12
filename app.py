d55cbcd0a0c83f0269e5ba97e34493064e4a72ac
ametepehounkponou-source <ametepe.hounkponou@gmail.com>
2026-05-12 11:50:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,7 @@
 import streamlit as st
-import pandas as pd
 
 st.title("🏆 Subae League AR")
 
-st.success("App connectée et fonctionnelle")
+st.write("Test de démarrage...")
 
-st.write("Dashboard en cours de construction...")
-
-# test tableau
-df = pd.DataFrame({
-    "Kuyok": ["KYK1", "KYK2", "KYK3"],
-    "Points": [10, 20, 15]
-})
-
-st.dataframe(df)
+st.success("Si tu vois ceci → app OK")
