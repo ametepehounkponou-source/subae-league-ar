dc7e31decb24f087d186b073d9d84b2e419599b5
ametepehounkponou-source <ametepe.hounkponou@gmail.com>
2026-05-12 12:16:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,14 +5,18 @@ st.set_page_config(page_title="Subae League AR")
 
 st.title("🏆 Subae League AR")
 
-# chargement
+# fichiers
 matchs = pd.read_csv("matchs.csv")
-resultats = pd.read_csv("resultats.csv")
+
+try:
+    resultats = pd.read_csv("resultats.csv")
+except:
+    resultats = pd.DataFrame()
 
 st.subheader("📅 Journée 1")
 st.dataframe(matchs)
 
-# barème
+# BARÈME
 points_map = {
     "Passage sur le field": 1,
     "Fruit Mannam fixé": 5,
@@ -25,15 +29,15 @@ points_map = {
     "Participation OT": 500
 }
 
-# calcul score
-def calcul_points(row):
+# calcul
+def calc(row):
     total = 0
     for k, v in points_map.items():
         total += row.get(k, 0) * v
     return total
 
 if not resultats.empty:
-    resultats["points"] = resultats.apply(calcul_points, axis=1)
+    resultats["points"] = resultats.apply(calc, axis=1)
 
     classement = resultats.groupby("kuyok")["points"].sum().reset_index()
     classement = classement.sort_values("points", ascending=False)
@@ -41,4 +45,4 @@ if not resultats.empty:
     st.subheader("🏆 Classement")
     st.dataframe(classement)
 else:
-    st.info("Aucun résultat pour le moment")
+    st.info("Aucun résultat encore")
