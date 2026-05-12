8999b85e638379ba9952c6d0adbb16ce533f600f
ametepehounkponou-source <ametepe.hounkponou@gmail.com>
2026-05-12 12:13:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,10 @@ st.set_page_config(page_title="Subae League AR")
 
 st.title("🏆 Subae League AR")
 
-st.write("🚀 App démarrée avec succès")
+st.write("Dashboard en ligne ✔️")
 
-# test tableau simple
-df = pd.DataFrame({
-    "Kuyok": ["KYK1", "KYK2", "KYK3"],
-    "Points": [10, 20, 15]
-})
+# chargement des matchs
+matchs = pd.read_csv("matchs.csv")
 
-st.dataframe(df)
+st.subheader("📅 Journée 1")
+st.dataframe(matchs)
